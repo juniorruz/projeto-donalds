4496a6bbfcc93aea948d6c6b6e244d0e21ae2550
Luiz Ruzenente <juniorruz95@gmail.com>
2025-02-21 10:18:47 -0300

    feat: add initial cart item component (without functionality)

--- src/app/[slug]/menu/[productId]/components/cart-sheet.tsx
+++ src/app/[slug]/menu/[productId]/components/cart-sheet.tsx
@@ -8,20 +8,22 @@ import {
 } from "@/components/ui/sheet";
 
 import { CartContext } from "../../contexts/cart";
+import CartProductItem from "./cart-product-item";
 
 const CartSheet = () => {
   const { isOpen, toggleCart, products } = useContext(CartContext);
   return (
     <Sheet open={isOpen} onOpenChange={toggleCart}>
-      <SheetContent>
+      <SheetContent className="w-[80%]">
         <SheetHeader>
-          <SheetTitle>Are you absolutely sure?</SheetTitle>
+          <SheetTitle className="text-left">Sacola</SheetTitle>
         </SheetHeader>
-        {products.map((product) => (
-          <h1 key={product.id}>
-            {product.name} {product.quantity}
-          </h1>
-        ))}
+        <div className="py-5">
+          {" "}
+          {products.map((product) => (
+            <CartProductItem key={product.id} product={product} />
+          ))}
+        </div>
       </SheetContent>
     </Sheet>
   );
