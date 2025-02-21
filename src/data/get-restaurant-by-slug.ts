2c52463cdd4facbe066038a7f1ebd1a6c359c9c8
Luiz Ruzenente <juniorruz95@gmail.com>
2025-02-21 16:30:51 -0300

    chore: sort file import

--- src/data/get-restaurant-by-slug.ts
+++ src/data/get-restaurant-by-slug.ts
@@ -1,9 +1,10 @@
 import { db } from "@/lib/prisma";
 
 export const getRestaurantBySlug = async (slug: string) => {
-  const restaurat = db.restaurant.findUnique({
+  const restaurant = db.restaurant.findUnique({
     where: {
       slug,
     },
   });
+  return restaurant;
 };
